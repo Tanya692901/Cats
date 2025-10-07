b0e094c03534922e716d81905eb4bac025709e6d
Tanya <rymyanceva1983@gmail.com>
2025-10-07 22:47:49 +0300

    Создана функция для получения изображения с котиком

--- Cats.py
+++ Cats.py
@@ -1,7 +1,27 @@
+from http.client import responses
 from tkinter import *
+from urllib import response
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+from urllib3.util import url
+
+
+def load_image():
+    try:
+        responses = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(responses.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"произошла ошибка: {e}")
+        return None
+
+
+
+
 
 window = Tk()
 window.title("Cats")
