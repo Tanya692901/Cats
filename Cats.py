6fbba70275569e2c83ba60f5ef25ec74b35b825d
Tanya <rymyanceva1983@gmail.com>
2025-10-07 22:36:16 +0300

    Создано окно и виджеты

--- Cats.py
+++ Cats.py
@@ -1,1 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title("Cats")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+Url = "https://cataas.com/cat"
+img = load.Image(Url)
+
+if img:
+    label.configure(image=img)
+    label.image = img
+
+window.mainloop()
+
+
+
+
